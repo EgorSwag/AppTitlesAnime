c14bbd3f9b72055e4bb4a7785f41ac64c50cad36
User <User@WIN-BT7OLJV0750>
2025-04-10 14:20:18 +0500

    Не завершен вывод типов аниме(2:13:21)

--- AppTitlesAnime/FormListTypes.Designer.cs
+++ AppTitlesAnime/FormListTypes.Designer.cs
@@ -50,7 +50,7 @@
             flowLayoutPanelTop.Location = new Point(0, 0);
             flowLayoutPanelTop.Name = "flowLayoutPanelTop";
             flowLayoutPanelTop.Padding = new Padding(10);
-            flowLayoutPanelTop.Size = new Size(1002, 61);
+            flowLayoutPanelTop.Size = new Size(934, 61);
             flowLayoutPanelTop.TabIndex = 0;
             // 
             // btnAddType
@@ -86,12 +86,16 @@
             // 
             // dataGridViewTypes
             // 
+            dataGridViewTypes.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataGridViewTypes.BackgroundColor = Color.White;
             dataGridViewTypes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridViewTypes.Dock = DockStyle.Fill;
             dataGridViewTypes.Location = new Point(10, 10);
+            dataGridViewTypes.MultiSelect = false;
             dataGridViewTypes.Name = "dataGridViewTypes";
-            dataGridViewTypes.Size = new Size(982, 416);
+            dataGridViewTypes.ReadOnly = true;
+            dataGridViewTypes.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridViewTypes.Size = new Size(914, 430);
             dataGridViewTypes.TabIndex = 1;
             // 
             // panelFill
@@ -101,20 +105,21 @@
             panelFill.Location = new Point(0, 61);
             panelFill.Name = "panelFill";
             panelFill.Padding = new Padding(10);
-            panelFill.Size = new Size(1002, 436);
+            panelFill.Size = new Size(934, 450);
             panelFill.TabIndex = 2;
             // 
             // FormListTypes
             // 
             AutoScaleDimensions = new SizeF(11F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1002, 497);
+            ClientSize = new Size(934, 511);
             Controls.Add(panelFill);
             Controls.Add(flowLayoutPanelTop);
             Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
             Margin = new Padding(5);
             Name = "FormListTypes";
             Text = "Список типов аниме";
+            Load += FormListTypes_Load;
             flowLayoutPanelTop.ResumeLayout(false);
             flowLayoutPanelTop.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridViewTypes).EndInit();
--- AppTitlesAnime/FormListTypes.cs
+++ AppTitlesAnime/FormListTypes.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppTitlesAnime.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,20 +9,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AppContext = AppTitlesAnime.Models.AppContext;
 
 namespace AppTitlesAnime
 {
     public partial class FormListTypes : Form
     {
+        private AppContext db;
+
         public FormListTypes()
         {
             InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            this.db = new AppContext();
+            this.db.Types.Load();
+            this.dataGridViewTypes.DataSource = this.db.Types.Local.OrderBy(o => o.TypeName).ToList();
+
+            // скрытие столбцов
+            dataGridViewTypes.Columns["Id"].Visible = false;
+            dataGridViewTypes.Columns["AnimeTitles"].Visible = false;
+
+            // изменение названий заголовков столбцов
+            dataGridViewTypes.Columns["TypeName"].HeaderText = "Тип аниме";
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
         }
 
         private void btnAddType_Click(object sender, EventArgs e)
         {
             FormAddType formAddType = new FormAddType();
             formAddType.ShowDialog();
+        }
+
+        private void FormListTypes_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
