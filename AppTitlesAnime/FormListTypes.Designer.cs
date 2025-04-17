fc7bbaaef3823481a06b300e172074ea6d468ecc
User <User@WIN-BT7OLJV0750>
2025-04-17 09:55:42 +0500

    Добавлено удаление строки из таблицы Типы аниме

--- AppTitlesAnime/FormListTypes.Designer.cs
+++ AppTitlesAnime/FormListTypes.Designer.cs
@@ -84,6 +84,7 @@
             btnDeleteType.TabIndex = 2;
             btnDeleteType.Text = "Удалить";
             btnDeleteType.UseVisualStyleBackColor = true;
+            btnDeleteType.Click += btnDeleteType_Click;
             // 
             // dataGridViewTypes
             // 
